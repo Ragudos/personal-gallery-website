1ad05ef11f0a6cb848d91912f349d9d5d8fe5270
Ragudos <workemailaaronragudos@gmail.com>
2023-08-14 02:00:45 +0800

    remove workbox

--- workbox.config.cjs
+++
@@ -1,9 +0,0 @@
-// A config for `generateSW`
-// globDirectory is for Netlify's dist location
-module.exports = {
-  globDirectory: '/opt/build/repo/dist/',
-  globPatterns: [
-    '**/*.{css,woff2,png,svg,jpg,js}'
-  ],
-  swDest: '/opt/build/repo/dist/sw.js'
-};
